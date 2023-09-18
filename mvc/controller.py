4a12e379cb25be195ff228cb385c2dcefd22436a
fosternilsson <fosternilsson99@gmail.com>
2023-09-18 17:10:37 -0500

    rendering result page but not grabbing zip

--- mvc/controller.py
+++ mvc/controller.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for
 from myapp import app
 from mvc.model import ZipCodeModel
 from mvc.view import ZipCodeForm, render_index, render_template
