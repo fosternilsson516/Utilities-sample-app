bb16f3dc7d654b751431c4e310894ea7734e16f4
fosternilsson <fosternilsson99@gmail.com>
2023-09-21 13:26:35 -0500

    added check for 5 digit zip code and at least one appliance selected

--- static/script.js
+++ static/script.js
@@ -11,15 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
         const zipCodeInput = document.getElementById("zipCodeInput");
         const zipCode = zipCodeInput.value;
 
-        console.log(zipCodeInput);
-        console.log(zipCode);
+        // Check if the zipCode is a 5-digit number using a regular expression
+        const zipCodePattern = /^\d{5}$/;
+        if (!zipCodePattern.test(zipCode)) {
+            // Display an error message or take appropriate action
+            alert("Please enter a valid 5-digit zip code.");
+            return; // Exit the function to prevent further processing
+        }
 
         const appliances = [];
         const applianceCheckboxes = document.querySelectorAll('input[name="appliances[]"]:checked');
         applianceCheckboxes.forEach(function (checkbox) {
             appliances.push(checkbox.value);
         });
-        console.log("Selected Appliances:", appliances);
+        
+        // Check if at least one appliance is selected
+        if (appliances.length === 0) {
+            // Display an error message or take appropriate action
+            alert("Please select at least one Appliance.");
+            return; // Exit the function to prevent further processing
+        }
+
         const csrfTokenInput = document.querySelector('input[name="csrf_token"]').value;
 
         const formData = new FormData();
@@ -38,7 +50,6 @@ document.addEventListener("DOMContentLoaded", function () {
         axios.post("/submit", formData)
         .then(function (response) {
             const data = response.data;
-
             if (data.redirect) {
                 // Redirect to the /result route
                 window.location.href = data.redirect;
@@ -51,27 +62,5 @@ document.addEventListener("DOMContentLoaded", function () {
             // Handle errors
             console.error("An error occurred:", error);
         });
-    
-            
-            // Handle the response data
-           // if (data.zipCode) {
-           //     zipCodeDiv.innerHTML = "zip code: " + data.zipCode;
-           // } else {
-            //    zipCodeDiv.innerHTML = "No result available.";
-          //  }
-
-            // Check if selected appliances data is available
-           // if (data.appliances && data.appliances.length > 0) {
-           //     selectedAppliancesDiv.innerHTML = "Selected Appliances: " + data.appliances.join(", ");
-           // } else {
-          //      selectedAppliancesDiv.innerHTML = "No appliances selected.";
-          //  }
-
-       // })
-         //   .catch(function (error) {
-                // Handle errors
-          //      zipCodeDiv.innerHTML = "An error occurred while processing the request.";
-         //       selectedAppliancesDiv.innerHTML = "No appliances selected.";
-         //   });
     }
 });
